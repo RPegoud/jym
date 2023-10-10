5c1149050a22d9ff842b3066b183d28c8d6dee2e
Ryan Pégoud <ryanpegoud@gmail.com>
2023-10-10 09:37:46 +0200

    pmap start

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import jax.numpy as jnp
-from jax import random
-from tqdm.auto import tqdm
-
 from agents import Q_learning
 from envs import GridWorld
+from jax import jit, random, vmap
 from policies import EpsilonGreedy
 
 SEED = 2
+N_ENV = 10
 GRID_DIM = jnp.array([8, 8])
 INITIAL_STATE = jnp.array([8, 8])
 GOAL_STATE = jnp.array([0, 0])
@@ -29,24 +28,49 @@ agent = Q_learning(
     policy,
 )
 
-q_values = jnp.zeros(
-    [GRID_SIZE[0], GRID_SIZE[1], N_ACTIONS],
-    dtype=jnp.float32,
-)
 movements = jnp.array([[0, 1], [1, 0], [0, -1], [-1, 0]])
 
-env_state, obs = env.reset(key)
-done = False
-steps = []
+v_reset = jit(
+    vmap(env.reset, out_axes=((0, 0), 0), axis_name="batch_axis")  # ((env_state), obs)
+)
 
-for _ in tqdm(range(400)):
-    done = False
-    n_steps = 0
-    while not done:
-        state, _ = env_state
-        action, key = policy(key, N_ACTIONS, q_values[tuple(state)])
-        movement = movements[action]
-        env_state, obs, reward, done = env.step(env_state, movement)
+v_step = jit(
+    vmap(
+        env.step,
+        in_axes=((0, 0), 0),  # ((env_state), action)
+        out_axes=((0, 0), 0, 0, 0),  # ((env_state), obs, reward, done)
+        axis_name="batch_axis",
+    )
+)
 
-        q_values = agent.update(state, action, reward, done, obs, q_values)
-        n_steps += 1
+v_policy = jit(
+    vmap(
+        policy.call,
+        in_axes=(0, None, 0, -1),  # (keys, n_actions, state, q_values)
+        axis_name="batch_axis",
+    ),
+    static_argnums=(1,),
+)
+
+v_update = jit(
+    vmap(
+        agent.update,
+        in_axes=(0, 0, 0, 0, 0, -1),
+        axis_name="batch_axis",
+    ),
+)
+
+
+keys = random.split(key, N_ENV)
+env_state, obs = v_reset(keys)
+states, keys = env_state
+q_values = jnp.zeros([GRID_SIZE[0], GRID_SIZE[1], N_ACTIONS, N_ENV], dtype=jnp.float32)
+
+action, keys = v_policy(keys, N_ACTIONS, states, q_values)
+
+env_state, obs, reward, done = v_step(env_state, action)
+states, keys = env_state
+
+q_values = v_update(states, action, reward, done, obs, q_values)
+
+print(states.shape, action.shape, reward.shape)
